81cfb740c117309b0662148266b1887f1c6bfa86
ema161 <ema161@uclive.ac.nz>
2024-10-03 17:00:17 +1300

    Replace current time picker dialog with advanced version to fix landscape layout issue

--- app/src/main/java/com/example/busapp/screens/RouteFinderScreen.kt
+++ app/src/main/java/com/example/busapp/screens/RouteFinderScreen.kt
@@ -1,16 +1,20 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -27,6 +31,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import java.util.Calendar
 
@@ -88,7 +94,7 @@ fun RouteFinder(navController: NavController) {
         }
 
         if (showDialog) {
-            DialWithDialog(
+            AdvancedTimePicker(
                 onConfirm = {
                     showDialog = false
                 },
@@ -115,12 +121,14 @@ fun RouteFinder(navController: NavController) {
     }
 }
 
+// Code snippet base taken from developer.android.com
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DialWithDialog(
+fun AdvancedTimePicker(
     onConfirm: (TimePickerState) -> Unit,
     onDismiss: () -> Unit,
 ) {
+
     val currentTime = Calendar.getInstance()
 
     val timePickerState = rememberTimePickerState(
@@ -129,9 +137,9 @@ fun DialWithDialog(
         is24Hour = false,
     )
 
-    TimePickerDialog(
+    AdvancedTimePickerDialog(
         onDismiss = { onDismiss() },
-        onConfirm = { onConfirm(timePickerState) }
+        onConfirm = { onConfirm(timePickerState) },
     ) {
         TimePicker(
             state = timePickerState,
@@ -139,25 +147,53 @@ fun DialWithDialog(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TimePickerDialog(
+fun AdvancedTimePickerDialog(
+    title: String = "Select Time",
     onDismiss: () -> Unit,
     onConfirm: () -> Unit,
-    content: @Composable () -> Unit
+    toggle: @Composable () -> Unit = {},
+    content: @Composable () -> Unit,
 ) {
-    AlertDialog(
+    Dialog(
         onDismissRequest = onDismiss,
-        dismissButton = {
-            TextButton(onClick = { onDismiss() }) {
-                Text("Cancel")
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
+            modifier =
+            Modifier
+                .width(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = MaterialTheme.colorScheme.surface
+                ),
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                content()
+                Row(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    toggle()
+                    Spacer(modifier = Modifier.weight(1f))
+                    TextButton(onClick = onDismiss) { Text("Cancel") }
+                    TextButton(onClick = onConfirm) { Text("OK") }
+                }
             }
-        },
-        confirmButton = {
-            TextButton(onClick = { onConfirm() }) {
-                Text("Confirm")
-            }
-        },
-        text = { content() }
-    )
+        }
+    }
 }
