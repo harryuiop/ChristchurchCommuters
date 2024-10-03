c8d11795de732c3889f35393f340936c332b2e75
ema161 <ema161@uclive.ac.nz>
2024-10-03 16:31:38 +1300

    Start adding upcoming routes section, and option to swap from Arrive or Leave by

--- app/src/main/java/com/example/busapp/screens/RouteFinderScreen.kt
+++ app/src/main/java/com/example/busapp/screens/RouteFinderScreen.kt
@@ -1,14 +1,18 @@
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerState
@@ -20,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -27,7 +32,8 @@ import java.util.Calendar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RouteFinder(navController: NavController, /*busStopViewModel: BusStopViewModel*/) {
+fun RouteFinder(navController: NavController) {
+    var checked by remember { mutableStateOf(false) }
     var showDialog by remember { mutableStateOf(false) }
 
     Column (
@@ -41,28 +47,45 @@ fun RouteFinder(navController: NavController, /*busStopViewModel: BusStopViewMod
         Spacer(modifier = Modifier.size(24.dp))
 
         OutlinedTextField(
-            value = "Choose start location",
+            value = "",
             onValueChange = {},
-            label = { Text("Start Location") }
+            label = { Text("Choose start location") }
         )
 
         Spacer(modifier = Modifier.size(12.dp))
 
         OutlinedTextField(
-            value = "Choose destination",
+            value = "",
             onValueChange = {},
-            label = { Text("Destination") }
+            label = { Text("Choose destination") }
         )
 
         Spacer(modifier = Modifier.size(12.dp))
 
-        OutlinedButton(onClick = { showDialog = true }) {
-            Text("Leave by")
+
+        Row(
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally).padding(horizontal = 20.dp)
+        ) {
+            OutlinedButton(
+                onClick = { showDialog = true },
+                modifier = Modifier.padding(end = 12.dp)
+            ) {
+                var timeOptionMsg = if (checked) {
+                    "Arrive by"
+                } else {
+                    "Leave by"
+                }
+
+                Text(timeOptionMsg)
+            }
+
+            Switch(
+                checked = checked,
+                onCheckedChange = {
+                    checked = it
+                }
+            )
         }
-
-        val currentTime = Calendar.getInstance()
-
-        Text(text = "${currentTime.get(Calendar.HOUR_OF_DAY)}:${Calendar.MINUTE} ${Calendar.AM}")
 
         if (showDialog) {
             DialWithDialog(
@@ -73,6 +96,21 @@ fun RouteFinder(navController: NavController, /*busStopViewModel: BusStopViewMod
                     showDialog = false
                 }
             )
+        }
+
+        Spacer(modifier = Modifier.size(24.dp))
+
+        Text(text = "Upcoming", fontSize = 12.sp)
+
+        LazyColumn(
+        ) {
+            item {
+                Text(
+                    text = "1",
+                    modifier = Modifier.fillMaxWidth().background(Color.White),
+                    color = Color.Black
+                )
+            }
         }
     }
 }
