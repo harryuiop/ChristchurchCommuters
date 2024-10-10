8fa926ca97d86e4b449e3a50c7abc8f6b682b1b2
ezeck <ema161@uclive.ac.nz>
2024-10-10 19:53:51 +1300

    Use string resources for Add Bus Stop screen string literals

--- app/src/main/java/com/example/busapp/screens/AddBusStopToHomeScreen.kt
+++ app/src/main/java/com/example/busapp/screens/AddBusStopToHomeScreen.kt
@@ -2,39 +2,29 @@ package com.example.busapp.screens
 
 import android.util.Log
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.Key.Companion.Search
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.busapp.models.BusStop
+import com.example.busapp.R
 import com.example.busapp.viewmodels.AddBusStopViewModel
 import com.example.busapp.viewmodels.UserViewModel
 
@@ -55,12 +45,12 @@ fun AddBusStop(
             .fillMaxWidth()
             .padding(horizontal = 20.dp),
     ) {
-        Text(text = "Metro Bus Stops", fontSize = 24.sp)
+        Text(text = stringResource(id = R.string.metro_bus_stops_title), fontSize = 24.sp)
         OutlinedTextField(
             value = addBusStopViewModel.userQuery,
             onValueChange = { newQuery -> addBusStopViewModel.updateQuery(newQuery) },
-            label = { Text("Search") },
-            placeholder = { Text("Type Stop # or Location") },
+            label = { Text(stringResource(id = R.string.label_search)) },
+            placeholder = { Text(stringResource(id = R.string.text_placeholder_type_stop)) },
             modifier = Modifier.fillMaxWidth()
         )
         LazyColumn(
@@ -76,9 +66,9 @@ fun AddBusStop(
                 ) {
                     OutlinedCard(
                         onClick = {
-                            Log.d("BusStop", value);
-                            addBusStopViewModel.updateSelectedBusStop(key.toInt(), value);
-                            Log.d("BusStop", addBusStopViewModel.selectedBusStop.toString());
+                            Log.d("BusStop", value)
+                            addBusStopViewModel.updateSelectedBusStop(key.toInt(), value)
+                            Log.d("BusStop", addBusStopViewModel.selectedBusStop.toString())
                             focusManager.clearFocus()
                         },
                         modifier = Modifier
@@ -99,7 +89,7 @@ fun AddBusStop(
         }
             Button(
                 onClick = {
-                    userViewModel.editUserById(busStop = addBusStopViewModel.selectedBusStop);
+                    userViewModel.editUserById(busStop = addBusStopViewModel.selectedBusStop)
                     addBusStopViewModel.updateSelectedBusStop(-1, "")
                     addBusStopViewModel.updateQuery("")
                     navController.navigate("Home")
@@ -108,7 +98,7 @@ fun AddBusStop(
                 modifier = Modifier.fillMaxWidth(),
                 enabled = addBusStopViewModel.selectedBusStop.id != -1
             ) {
-                Text(text = "Add to Home")
+                Text(text = stringResource(id = R.string.button_add_to_home))
             }
 
     }
