16822011893980c2095a2dfcf661afa5c66e494d
ezeck <ema161@uclive.ac.nz>
2024-10-11 15:56:02 +1300

    Parcelize

--- app/src/main/java/com/example/busapp/models/BusNotificationState.kt
+++ app/src/main/java/com/example/busapp/models/BusNotificationState.kt
@@ -1,7 +1,11 @@
 package com.example.busapp.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class BusNotificationState(
     var isNotificationEnabled: Boolean = false,
     var hasFiveMinNotified: Boolean = false,
     var hasOneMinNotified: Boolean = false
-)
+) : Parcelable
